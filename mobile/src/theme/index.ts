39f15864a74757b4edbbdf187b41d2d083c80417
joshuamcnabb <joshua@lc.dev>
2022-03-05 00:13:04 -0700

    add fonts

--- mobile/src/theme/index.ts
+++ mobile/src/theme/index.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from 'native-base';
 import { colors } from './colors';
+import { fontConfig, fonts } from './fonts';
 
-export const theme = extendTheme({ colors });
+export const theme = extendTheme({ colors, fontConfig, fonts });
